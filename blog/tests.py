b80c7cd862f7cf425ab1250b26a07aefbd6105af
diego <rodriguezvargasd214@gmail.com>
2023-07-23 10:23:11 -0700

    Crud completed

--- blog/tests.py
+++ blog/tests.py
@@ -46,4 +46,25 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Titulo de prueba")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_createview(self):
+        response = self.client.post(reverse("post_new"),
+                                   {"title": "Test title", 
+                                       "body": "Test body", 
+                                       "author": self.user.id},)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Test title")
+        self.assertEqual(Post.objects.last().body, "Test body")
+
+    def test_post_updateview(self):
+        response = self.client.post(reverse("post_edit", args="1"), 
+                                    {"title": "Updated title", 
+                                     "body": "Updated body"},)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated body")
+
+    def test_post_deleteview(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
         
